96ef7101d8e6c39a77e36ac5ad037e2c2ba8ea7a
ChristianCoons <christiananthonycoons@gmail.com>
2025-02-03 16:02:41 -0700

    local changes to longestPalindrome.py

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -1,1 +1,7 @@
-#test text
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        for i in range(0, len(s)):
+            print(s[i])
+
+solution = Solution()
+solution.longestPalindrome("test")
