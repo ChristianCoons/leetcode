337a9780c24be459f05d046bfbb5449a2d2b22b0
ChristianCoons <christiananthonycoons@gmail.com>
2025-02-13 09:35:19 -0700

    RemoveDuplicates_2.cs

--- CSharp/obj/Debug/net8.0/CSharp.AssemblyInfo.cs
+++ CSharp/obj/Debug/net8.0/CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd441e712e607739cef9efb087fd98d98d018886")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbc64dcafe12b5b03d87d01695b53b6f0e0a04f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
